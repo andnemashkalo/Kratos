dc019068e5fa6a29f7b48dc2ded3b71c7364a053
andnemashkalo <alfonso8and@gmail.com>
2018-05-05 13:48:33 +0300

    test tasks

--- src/com/Practice_4/Test_11.java
+++ src/com/Practice_4/Test_11.java
@@ -12,7 +12,6 @@ public class Test_11 {
         int[] arr1={0,0,0,0,0,0,0,0};
         System.out.println("arr0 = " + Arrays.toString(arr0));
         System.out.println("arr1 = " + Arrays.toString(arr1));
-
         System.arraycopy(arr0,1,arr1,2,3);
         System.out.println("arr1 = " + Arrays.toString(arr1));
             }
